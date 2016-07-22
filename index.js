f85dc1f8ad005b1e6885aec7ebe30644219275dc
Aviad Hahami <aviadhahami@gmail.com>
2016-07-22 16:05:06 +0300

    modifications to test and index to fit into the non-supporting ES6 node

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
  * Created by aviad on 7/22/2016.
  */
 'use strict';
+class JoltScheduler{
+	
+	run(){
+		console.log('hi');
+	}
+	
+}
 
-
+module.exports = JoltScheduler
---
+++ test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by aviad on 7/22/2016.
+ */
+
+'use strict';
+let JoltScheduler = require('./index')
+
+let sc = new JoltScheduler();
+sc.run();
