e14b7a297969ab1e89c24a4fd4097d6866b94974
Aviad Hahami <aviadhahami@gmail.com>
2016-07-22 17:05:41 +0300

    Added new generate id function + shape verifier for entries

--- hashed-min-heap.js
+++ hashed-min-heap.js
@@ -5,27 +5,55 @@
 'use strict'
 
 let MinHeap = require('min-heap');
-class HashedMinHeap{
 
-	constructor(){
-		
-		this.minHeap = new MinHeap((a,b)=>{
+class HashedMinHeap{
+	
+	constructor() {
+		this.minHeap = new MinHeap((a, b)=> {
 			// Since we have specified form, we touch the schedule time directly
 			return a.st - b.st
 		});
+		this.hashTable = {}
 	}
 	
-	get pop(){
+	get pop() {
 		
 	}
-	set insert(obj){
+	
+	// When inserting new entry, we inject the object unique id
+	// in a way we will couple the hash to the heap
+	set insert(entry) {
+		if(this.verifyShape(entry)){
+			
+		}else{
+			throw 'Entry is not of proper form!'
+		}
+	}
+	
+	verifyShape(entry) {
+		return entry.hasOwnProperty('name') &&
+			typeof entry.name == 'string' &&
+			entry.hasOwnProperty('st') &&
+			typeof entry.st == 'number' &&
+			entry.hasOwnProperty('callback') &&
+			typeof entry.callback == 'function'
+	}
+	
+	get peek() {
 		
 	}
-	get peek(){
+	
+	set modifyEntry(entry) {
 		
 	}
-	set modifyEntry(obj){
+	
+	generateID(startingTime) {
 		
+		// Referenced from https://gist.github.com/gordonbrander/2230317
+		// Math.random should be unique because of its seeding algorithm.
+		// Convert it to base 36 (numbers + letters), and grab the first 7 characters
+		// after the decimal.
+		return '_' + Math.random().toString(36).substr(2, 7) + startingTime.toString();
 	}
 }
 
