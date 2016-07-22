1b1e9b92345ad467fa5311674cd5573796786f8d
Aviad Hahami <aviadhahami@gmail.com>
2016-07-22 16:52:54 +0300

    implementing new dataSet

--- hashed-min-heap.js
+++ hashed-min-heap.js
@@ -4,8 +4,29 @@
 
 'use strict'
 
+let MinHeap = require('min-heap');
 class HashedMinHeap{
+
+	constructor(){
+		
+		this.minHeap = new MinHeap((a,b)=>{
+			// Since we have specified form, we touch the schedule time directly
+			return a.st - b.st
+		});
+	}
 	
+	get pop(){
+		
+	}
+	set insert(obj){
+		
+	}
+	get peek(){
+		
+	}
+	set modifyEntry(obj){
+		
+	}
 }
 
 module.exports = HashedMinHeap
